f10d17116bb81e4fd99fec821b3b007c8fccf893
Jakub Nowak <nowakkuba99@gmail.com>
2023-01-26 09:30:48 +0100

    First commit

---
+++ src/API.cpp
@@ -0,0 +1,17 @@
+//
+//  API.cpp
+//  AppToGIF
+//
+//  Created by Jakub Nowak on 26/01/2023.
+//
+
+#include "API.hpp"
+#include <iostream>
+
+int main()
+{
+    
+    std::cout<<" a \n";
+    
+    return 0;
+}
---
+++ src/API.hpp
@@ -0,0 +1,13 @@
+//
+//  API.hpp
+//  AppToGIF
+//
+//  Created by Jakub Nowak on 26/01/2023.
+//
+
+#ifndef API_hpp
+#define API_hpp
+
+#include <stdio.h>
+
+#endif /* API_hpp */
