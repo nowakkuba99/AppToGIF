7710d94c9a3d2c1b376d6294a71b9933c24f0ad9
Jakub Nowak <nowakkuba99@gmail.com>
2023-02-10 16:13:06 +0100

    Added check if encoder is initialized before it can be runned

--- src/EncoderApp/EncoderApp.cpp
+++ src/EncoderApp/EncoderApp.cpp
@@ -14,21 +14,45 @@ namespace AppToGIF
 //Initialize FrameBuffer and set video settings
 void EncoderApp::init(AppToGIF::GIFSettings settings)
 {
+    AppToGIF::ErrorReporter err;
     std::unique_lock<std::mutex> lock(m_Mutex);
     /* Creation of member variables */
+    if(settings.frameRate>100)
+    {
+        settings.frameRate=100;
+        std::cerr<<"Max frame rate for gif files is 100.\n";
+    }
     p_Encoder = std::make_unique<Encoder>(settings);    //Create encoder object
     p_FrameBuffer = std::make_unique<FrameBuffer>();    //Create FrameBuffer
     
     /* Initialization of member variables */
-    p_Encoder->init();
+    err = p_Encoder->init();
+    if(err == ErrorReporter::NoError)
+    {
+        m_initialized = true;
+    }
+    else
+    {
+        m_initialized = false;
+        std::cerr<<"Error while initializing encoder! \n";
+    }
 }
 
 //Start the encoderApp - start second thread
 void EncoderApp::startEncoder()
 {
     std::unique_lock<std::mutex> lock(m_Mutex);
-    m_running = true;
-    p_WorkerThread = std::make_unique<std::thread>(&AppToGIF::EncoderApp::worker,this);
+    if(m_running)
+        std::cerr<<"Encoder already running!\n";
+    else if(m_initialized)
+    {
+        m_running = true;
+        p_WorkerThread = std::make_unique<std::thread>(&AppToGIF::EncoderApp::worker,this);
+    }
+    else if(!m_initialized)
+    {
+        std::cerr<<"Encoder not initialized! \n";
+    }
 }
 
 //Stop the encoderApp when file is ready
