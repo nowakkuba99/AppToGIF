5bb100aa03412e2c8580f4b65edb7e21000ee9f4
Jakub Nowak <nowakkuba99@gmail.com>
2023-02-13 21:39:31 +0100

    Create setter and getter for asynchonous mode

--- src/EncoderApp/EncoderApp.hpp
+++ src/EncoderApp/EncoderApp.hpp
@@ -60,6 +60,10 @@ public:
         return p_FrameBuffer->commitFrame();
     }
     
+    /* --- Encoder mode setter and getter --- */
+    inline bool getIfModeAsynchonous() const {return m_asynchronousMode;};
+    inline void setAsynchronousMode() {m_asynchronousMode = true;};
+    
 private:
     /* --- Private functions --- */
     //Function executed by worker thread
@@ -102,6 +106,7 @@ private:
     /* --- App run variable --- */
     bool m_running = false;
     bool m_initialized = false;
+    bool m_asynchronousMode = false;
     /* --- Error reporting variable --- */
     AppToGIF::ErrorReporter m_error = AppToGIF::ErrorReporter::NoError;
     
