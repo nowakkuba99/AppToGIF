1a708edf30ac769200f091d1fc0fccaea4f59230
Jakub Nowak <nowakkuba99@gmail.com>
2023-02-15 21:50:29 +0100

    Working on demo app files

--- AppToGIF_Demo/main.cpp
+++ AppToGIF_Demo/main.cpp
@@ -12,7 +12,8 @@
 /* --- Main function --- */
 int main()
 {
-    const int lineWidth = av_image_get_linesize(AV_PIX_FMT_BGRA, 255, 0);
+    //const int lineWidth = av_image_get_linesize(AV_PIX_FMT_BGRA, 255, 0);
+    const int lineWidth = av_image_get_linesize(AV_PIX_FMT_RGB8, 255, 0);
 
     AppToGIF::EncoderApp& app = AppToGIF::EncoderApp::getInstance();
     AppToGIF::GIFSettings set;
@@ -24,29 +25,35 @@ int main()
     set.bitRate = 100000;
     set.frameRate = 50;
     set.bitRate = 10000;
+    //set.inputAlpha = true;
+    set.directEncoding = true;;
+    set.bgraEncoding = false;
+    set.doubleEncoding = false;
     app.init(set);
     app.startEncoder();
     std::shared_ptr<AppToGIF::Frame> appFrame;
     app.setAsynchronousMode();
-    for(int i = 0; i<255; i+=2)
+    int counter = 0;
+    for(int i = 0; i<255; i++)
     {
         int sin_i = std::lroundf(255 * (0.5 + 0.5 * std::sin(i * 0.01)));
         appFrame = app.getFrame();
         if(appFrame != nullptr)
         {
             {
-                std::cout<<"Frame: "<<i<<"\n";
+                std::cout<<"Frame: "<<++counter<<"\n";
                 appFrame->m_width=255;
                 appFrame->m_height=255;
-                appFrame->m_rgb = new uint8_t[static_cast<size_t>(255)*lineWidth];
-                for (int y = 0; y < 255; ++y) {
+                appFrame->m_rgb = new uint8_t[static_cast<size_t>(set.outputHeight)*lineWidth];
+                for (int y = 0; y < set.outputHeight ; y++) {
                     uint8_t *row = &appFrame->m_rgb[y * lineWidth];
-                    for (int x = 0; x < 255; x++) {
-                        const int index = x*4;
-                        row[index + 0] = ((x+(i))&0xFF); // b
-                        row[index + 1] = ((sin_i)&0xFF);//((y+(i))&0xFF); // g
-                        row[index + 2] = 0; // r
-                        row[index + 3] = 0xFF; // a
+                    for (int x = 0; x < set.outputWidth; x++) {
+                        //const int index = x*4;  //4 for alpha channel
+                        row[x] = (sin_i)&255;//((x+(i))&0xFF); //
+//                        row[index + 0] = ((sin_i+(i))&0xFF);    //b
+//                        row[index + 1] = ((128)&0xFF);    //g
+//                        row[index + 2] = ((sin_i+(i))&0xFF);    //r
+//                        row[index + 3] = 0xFF; // a
                     }
                 }
             }
@@ -67,4 +74,9 @@ int main()
 
 /* Notes */
 // No conversion option - straight to output format TO DO NOW
+// Build in features like:
+// - setting frame width and height
+// - appFrame->getrow(),
+// - automatic linewidth calcuation
+// Fix issue with black line at the bottom
 // Create build option with cmake
